a9ed49d3b0336ac8a78da35478091c1f2f3ff299
Kylie Chang <kyliechang@Kylies-MacBook-Air-2.local>
2022-06-26 11:39:31 -0700

    initialize next app, login/user logic

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+export default function Login() {
+  
+    return (
+      <div>
+        <h1> this is the login page </h1>
+      </div>
+    )
+  }
+  
+  
+  
---
+++ pages/signup.js
@@ -0,0 +1,12 @@
+export default function Signup() {
+  
+    return (
+      <div>
+        <h1> this is the signup page </h1>
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    )
+  }
+  
+  
+  
